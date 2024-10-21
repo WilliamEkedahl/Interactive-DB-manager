e04e86575e4b7d621706acd6cb7e375e4f1446de
WilliamEkedahl <167907468+WilliamEkedahl@users.noreply.github.com>
2024-10-21 16:21:59 +0200

    security Update dataAdd-courses.php

--- public_html/prg120V/innleverering2/dataAdd-courses.php
+++ public_html/prg120V/innleverering2/dataAdd-courses.php
@@ -1,5 +1,6 @@
 <?php
-session_start();                                                                                                
+session_start();                               
+global $conn;
 //check if the user is logged in
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
